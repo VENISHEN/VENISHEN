5a9d4d0dce22609af992fb76e510440d2bc7f18d
VENISHEN <venishen.ofc@gmail.com>
2026-01-31 15:18:49 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/user/<name>')
+def index(name):
+    if name.lower() == "admin":
+        return render_template('admin.html')
+    else:
+        return render_template('guest.html')
 
 if __name__ == '__main__':
     app.run()
